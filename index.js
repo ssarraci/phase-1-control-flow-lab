c294b2be71ad5d1bb346b9b51b830e43e23528b6
Storm <storm.sarracino01@gmail.com>
2021-06-08 12:05:49 -0500

    commit tree 909dc485c79c2d028c4c0029c253fce06f99f373

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  // this is where we can use conditionals given our argument, someValue
+  //don't forget to return whatever the result is!
+  let result;
+  if (someValue <= 400) {
+      result = 'This one is on me!'; 
+    }
+    else if (someValue > 2000 && someValue < 2500) {
+      result = 'I will gladly take your thirty bucks.';
+    }
+    
+    else if (someValue > 2500) {
+      result = 'No can do.';
+    }
+    return result;
+}
+ 
+function ternaryCheckCity(someCity){
+  //city, 2 different things
+  return someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // tip, 3 things. else 
+  let resultTip;
+  switch (tip) {
+    case 'generous':
+      resultTip = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      resultTip = 'Thank you.';
+      break;
+    default:
+      resultTip = 'Bye.'
+  }
+  return resultTip;
 }
